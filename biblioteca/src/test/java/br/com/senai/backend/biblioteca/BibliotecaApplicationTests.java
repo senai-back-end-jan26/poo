1b18bae5ed68ecdc2c21371882a071b545c5a3e4
debysouza <debsdebbie90@gmail.com>
2026-02-04 11:49:12 -0300

    projeto maven

---
+++ biblioteca/src/test/java/br/com/senai/backend/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.backend.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
